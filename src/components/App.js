502927fc5cdefbe8e5de01f110b1acfb7720b5b1
t.mittlener <t.mittlener@gmail.com>
2017-03-29 10:36:26 +0200

    feat: add lodash

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ require('normalize.css/normalize.css');
 import { Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import SecondPage from '../pages/SecondPage';
-
+import _ from 'lodash'
 
 import React from 'react';
 
 
 class App extends React.Component {
+
+
   render() {
     return (
       <div className="wrapper">
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,14 +7,19 @@ import {Link} from 'react-router-dom'
 class HomePage extends React.Component {
   constructor(){
     super();
-
-  }
+   }
 
   render(){
+    let helloCollection = [];
+    _.times(2,()=>{
+      helloCollection.push('hello')
+    });
     return(
     <div>
+      {helloCollection}
       <span>TEST</span>
       <Link to="/secondPage">Second Page</Link>
+
     </div>)
   }
 
